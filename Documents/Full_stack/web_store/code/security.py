41827e2525c864cf0b1daa58c521f6b2e5851700
amazedarc <amazed.arc@outlook.com>
2020-12-17 11:32:34 +0200

    implementing parser

--- Documents/Full_stack/web_store/code/security.py
+++ Documents/Full_stack/web_store/code/security.py
@@ -1,19 +1,12 @@
 from user import User
 
 
-users = [User(1, 'mazina', 'mazina')]
-
-username_mapping = {u.username: u for u in users}
-
-userid_mapping = {u.id: u for u in users}
-
-
 def authenticate(username, password):
-    user = username_mapping.get(username, None)
+    user = User.find_by_username(username)
     if user and user.password == password:
         return user
 
 
 def identity(payload):
     userid = payload['identity']
-    return userid_mapping.get(userid, None)
+    return User.find_by_id(userid)
---
+++ Documents/Full_stack/web_store/test.py
@@ -0,0 +1,23 @@
+import sqlite3
+
+connection = sqlite3.connect('data.db')
+
+cursor = connection.cursor()
+
+create_table = "CREATE TABLE users(id int, username text,password text)"
+cursor.execute(create_table)
+
+user = (1, 'mazina', 'mazina')
+insert_user = "INSERT INTO users VALUES(?,?,?)"
+cursor.execute(insert_user, user)
+
+users = [(2, 'davy', 'davy'), (3, 'arcel', 'arcel')]
+insert_user = "INSERT INTO users VALUES(?,?,?)"
+cursor.executemany(insert_user, users)
+
+select_query = 'SELECT * FROM users'
+for row in cursor.execute(select_query):
+    print(row)
+
+connection.commit()
+connection.close()
